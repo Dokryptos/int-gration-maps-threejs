dcd6d4b7f5eb0ca1e21d41e63e3ce8ff0f5c463a
Pierre Dok <pierredok@outlook.fr>
2024-10-14 23:21:40 +0200

    render 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Map from './pages/map'
+import {Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      <Routes>
+        <Route path='/' element={<Map/>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/map.tsx
@@ -0,0 +1,52 @@
+import { useRef, useState } from "react";
+import * as THREE from "three";
+import { Canvas } from "@react-three/fiber";
+import { MapControls } from "@react-three/drei";
+
+interface MapPlanerProps{
+    zoom: number
+}
+
+const MapPlane: React.FC<MapPlanerProps> = ({ zoom }) => {
+  const ref = useRef<THREE.Mesh>(null);
+
+  // texture SVG 
+  const texture = new THREE.TextureLoader().load("./public/images/map.svg");
+
+  return (
+    <mesh ref={ref} scale={[zoom, zoom, 1]}>
+      <planeGeometry args={[10, 5]} /> 
+      <meshBasicMaterial map={texture} />
+    </mesh>
+  );
+};
+
+const MapViewer = () => {
+  const [zoom, setZoom] = useState(1); // Zoom setting
+
+  return (
+    <div style={{ position: "relative", height: "100vh", width: "100vw" }}>
+      <Canvas>
+        {/* light */}
+        <ambientLight intensity={0.8} />
+        <pointLight position={[10, 10, 10]} />
+        <MapPlane zoom={zoom} />
+
+        {/* voir orbitcontrol pour juste zoomer et pas bouger*/}
+        <MapControls enableRotate={false} />     
+    </Canvas>
+
+      {/* button setting */}
+      <div style={{ position: "absolute", top: "10px", right: "10px", zIndex: 1 }}>
+        <button onClick={() => setZoom((prev) => Math.min(prev + 0.1, 5))}>
+          Zoom In
+        </button>
+        <button onClick={() => setZoom((prev) => Math.max(prev - 0.1, 0.5))}>
+          Zoom Out
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default MapViewer;
